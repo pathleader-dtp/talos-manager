51ae3c96e8982c1179bcc14b91f644f63981c3b1
Lars Trey <admin@treyla.de>
2024-04-11 10:47:08 +0300

    update talos and kubernetes versions

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -27,8 +27,8 @@ class ConfigsController < ApplicationController
 
   def new
     @config = Config.new(
-      kubernetes_version: "1.29.2",
-      install_image: "ghcr.io/siderolabs/installer:v1.6.6",
+      kubernetes_version: "1.29.3",
+      install_image: "ghcr.io/siderolabs/installer:v1.6.7",
       kubespan: true,
     )
   end
